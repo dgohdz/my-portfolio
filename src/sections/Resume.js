72f6818f50a9aadf3d2ece0cb7422d194f98b847
Diego Hernandez <diegohmailbox@gmail.com>
2021-09-01 14:25:43 -0700

    Still working on resume section.

--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import '../stylesheets/Resume.css'
 
 export default class Resume extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="Resume">
+                <div>
+                <h1>Education</h1>
+                <h3>What I've learned</h3>
+                </div>
+                <div>
+                <h1>Experience</h1>
+                <h3>Where I've worked</h3>
+                </div>
+                <div>
+                    <h1>Skills</h1>
+                    <h3>What I bring to the table</h3>
+                </div>
             </div>
         )
     }
