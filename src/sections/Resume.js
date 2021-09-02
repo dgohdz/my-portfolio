4c7dc0897406e2109d747fcdf9bca0da8a2b7bbe
Diego Hernandez <diegohmailbox@gmail.com>
2021-09-01 19:50:44 -0700

    Figuring out how to position Resume section.

--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react'
 import '../stylesheets/Resume.css'
 
 export default class Resume extends Component {
-    render() {
-        return (
-            <div className="Resume">
-                <div>
-                <h1>Education</h1>
-                <h3>What I've learned</h3>
-                </div>
-                <div>
-                <h1>Experience</h1>
-                <h3>Where I've worked</h3>
-                </div>
-                <div>
-                    <h1>Skills</h1>
-                    <h3>What I bring to the table</h3>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Resume">
+        <div className="Education">
+          <h1>Education</h1>
+          <h3>What I've learned</h3>
+        </div>
+        <div className="Experience">
+          <h1>Experience</h1>
+          <h3>Where I've worked</h3>
+        </div>
+        <div className="Skills">
+          <h1>Skills</h1>
+          <h3>What I bring to the table</h3>
+        </div>
+      </div>
+    )
+  }
 }
