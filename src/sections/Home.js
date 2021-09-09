2c92c1205d103ae93ce9be5e14d071d6d0ea527c
Diego Hernandez <diegohmailbox@gmail.com>
2021-09-08 18:46:49 -0700

    Need to fix Home section

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -4,9 +4,15 @@ import '../stylesheets/Home.css'
 export default class Home extends Component {
     render() {
         return (
-            <div className='HomeTitle'>
-                <h1>Hello, I'm Diego Hernandez</h1>
-                <h3>A Front-End Developer</h3>
+            <div className='Home-Section'>
+                <div className='Home-Container'>
+                <h3 className='Name'>Diego Hernandez</h3>
+                <p className='Job-Title'>Front-end Developer</p>
+                <h1 className='Greeting-Heading'>Hello! I'm a creative front-end developer looking forward to working with you.</h1>
+                <button className='Scroll-Button'>Scroll Down</button>
+                </div>
+
+
             </div>
         )
     }
