bd520798c0c2c92f109b646377f0133d8b8752b0
Diego Hernandez <diegohmailbox@gmail.com>
2021-09-16 17:48:56 -0700

    working on positioning with CSS

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -4,15 +4,11 @@ import '../stylesheets/Home.css'
 export default class Home extends Component {
     render() {
         return (
-            <div className='Home-Section'>
-                <div className='Home-Container'>
-                <h3 className='Name'>Diego Hernandez</h3>
-                <p className='Job-Title'>Front-end Developer</p>
-                <h1 className='Greeting-Heading'>Hello! I'm a creative front-end developer looking forward to working with you.</h1>
-                <button className='Scroll-Button'>Scroll Down</button>
-                </div>
-
-
+            <div className='home-container'>
+                <h1 className='name'>Diego Hernandez</h1>
+                <p className='job-title'>Front-end Developer</p>
+                <h1 className='greeting-heading'>Hello! I'm a creative front-end developer looking forward to working with you.</h1>
+                <button className='scroll-button'>Scroll Down</button>
             </div>
         )
     }
--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -4,18 +4,21 @@ import '../stylesheets/Resume.css'
 export default class Resume extends Component {
   render() {
     return (
-      <div className="Resume">
-        <div className="Education">
+      <div className="resume">
+        <div className="education">
           <h1>Education</h1>
           <h3>What I've learned</h3>
+          <p>Schools</p>
         </div>
-        <div className="Experience">
+        <div className="experience">
           <h1>Experience</h1>
           <h3>Where I've worked</h3>
+          <p>Jobs</p>
         </div>
-        <div className="Skills">
+        <div className="skills">
           <h1>Skills</h1>
           <h3>What I bring to the table</h3>
+          <p>List of skills</p>
         </div>
       </div>
     )
