0e1a83b7dbabb64ac78b641643723fc4bd5dd0f4
Diego Hernandez <diegohmailbox@gmail.com>
2021-09-24 18:47:34 -0700

    need to set up resume button

--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -8,8 +8,8 @@ export default class Resume extends Component {
       <div className="resume-container">
         <div className="skills">
           <h1>Skills</h1>
-          <h3>What I know</h3>
-          <ul>
+          <h2>What I know</h2>
+          <ul className="skills-list">
             <li>HTML</li>
             <li>CSS/SCSS</li>
             <li>Javascript</li>
@@ -19,12 +19,15 @@ export default class Resume extends Component {
         </div>
         <div className="experience">
           <h1>Experience</h1>
-          <h3>Where I've worked</h3>
-          <p>Jobs</p>
+          <h2>Where I've worked</h2>
+          <h3>US Army</h3>
+          <h3>Real Estate Agent</h3>
+          <h3>Notch8 Intern</h3>
+
         </div>
       </div>
       <div className="button-container">
-      <button className="download-resume">Download Resume</button>
+      <button className="download-resume" onClick="https://drive.google.com/file/d/13203KjaitOUzQSXxXui8HqRx1kIv_g5E/view?usp=sharing">Download Resume</button>
       </div>
       </>
     )
