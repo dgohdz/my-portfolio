e25f20332db80ede03756b09a3c1314939f14646
Diego Hernandez <diegohmailbox@gmail.com>
2021-10-03 17:03:30 -0700

    updated resume section

--- src/sections/Resume.js
+++ src/sections/Resume.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react'
 import '../stylesheets/Resume.css'
+import HTML from "../assets/images/html-icon.png";
+import CSS from "../assets/images/css-icon.png";
+import SASS from "../assets/images/sass-icon.png";
+import Javascript from "../assets/images/js-icon.png";
+import reactIcon from "../assets/images/react-icon.png";
+import vscode from "../assets/images/vscode-icon.png";
+import git from "../assets/images/git-icon.png";
 
 export default class Resume extends Component {
   render() {
     return (
       <>
       <section className="resume-section" id="Resume">
-
+          <div className="skills-container">
           <h1 className="skills-heading">Skills</h1>
           <h4 className="skills-subheading">What I know</h4>
-      <div className="list-container">
           <ul className="skills-grid">
-            <li>HTML</li>
-            <li>CSS/SCSS</li>
-            <li>Javascript</li>
-            <li>React.js</li>
-            <li>VS Code</li>
-            <li>Git</li>
+            <li>
+              <div><img src={HTML} alt="html-icon" className="html-icon"/>
+              <p>HTML</p>
+              </div>
+            </li>
+            <li>
+              <div><img src={CSS} alt="css-icon" className="css-icon"/>
+              <p>CSS</p>
+              </div>
+            </li>
+            <li>
+              <div><img src={SASS} alt="sass-icon" className="sass-icon"/>
+              <p>SASS</p>
+              </div>
+            </li>
+            <li>
+              <div><img src={Javascript} alt="js-icon" className="js-icon"/>
+              <p>Javascript</p>
+              </div>
+            </li>
+            <li>
+              <div><img src={reactIcon} alt="react-icon" className="react-icon"/>
+              <p>React.js</p>
+              </div>
+            </li>
+            <li>
+              <div><img src={vscode} alt="vscode-icon" className="vscode-icon"/>
+              <p>VS Code</p>
+              </div>
+            </li>
+            <li>
+              <div><img src={git} alt="git-icon" className="git-icon"/>
+              <p>Git</p>
+              </div>
+            </li>
           </ul>
-      </div>
+          </div>
+
       <div className="experience-container">
           <h1 className="experience-heading">Experience</h1>
           <h4 className="experience-subheading">Where I've worked</h4>
