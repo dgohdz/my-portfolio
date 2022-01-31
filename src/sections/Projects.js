bb27dc05d31fb079785c5d622d0a8511b147b19f
Diego Hernandez <diegohmailbox@gmail.com>
2022-01-31 12:17:53 -0800

    updated blackout image opacity

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -7,7 +7,7 @@ export default class Projects extends Component {
         return (
             <>
             <section className="project-section" id="Projects">
-            <div className="image-container">
+            <div className="project-container">
             <a href="https://infinite-taiga-38805.herokuapp.com/" target="_blank" rel="noreferrer"><img src= {Blackout} alt="" className="blackout-png"/></a>
                 <h1 className="blackout-heading">Blackout Project</h1>
                 <p className="instruction-paragraph">Click on the image to open the app.</p>
